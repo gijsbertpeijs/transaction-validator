e33f72fc22d3fe1ddf59b79b7ec1f070b25041db
Gijsbert Peijs <gijsbert.peijs@gmail.com>
2022-11-08 12:44:24 +0100

    Minor clean-ups

--- src/main/java/nl/rabodemo/transactionvalidator/TransactionItemProcessor.java
+++ src/main/java/nl/rabodemo/transactionvalidator/TransactionItemProcessor.java
@@ -7,13 +7,12 @@ import org.springframework.batch.item.ItemProcessor;
 
 import java.util.Objects;
 
-
 public class TransactionItemProcessor implements ItemProcessor<Transaction, Transaction> {
 
     private static final Logger log = LoggerFactory.getLogger(TransactionItemProcessor.class);
 
     @Override
-    public Transaction process(final Transaction transaction) throws Exception {
+    public Transaction process(final Transaction transaction) {
         if (isInvalidTransaction(transaction)) {
             transaction.setValid(0);
             log.error("Invalid transaction: {}", transaction);
