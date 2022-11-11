515c69ad19957d67e31abd22573fcbc7097a3966
Gijsbert Peijs <gijsbert.peijs@gmail.com>
2022-11-11 10:03:28 +0100

    Clean-up unneeded items in the tests.

--- src/test/java/nl/rabodemo/transactionvalidator/TransactionValidatorBatchFailedTest.java
+++ src/test/java/nl/rabodemo/transactionvalidator/TransactionValidatorBatchFailedTest.java
@@ -4,7 +4,6 @@ import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.read.ListAppender;
 import org.junit.After;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,7 +15,6 @@ import org.springframework.batch.test.JobRepositoryTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
@@ -24,7 +22,6 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
-import org.springframework.test.jdbc.JdbcTestUtils;
 
 import java.util.List;
 
@@ -47,9 +44,6 @@ class TransactionValidatorBatchFailedTest {
     @Autowired
     private JobRepositoryTestUtils jobRepositoryTestUtils;
 
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
-
     private ListAppender<ILoggingEvent> listAppender;
 
     @BeforeEach
@@ -58,11 +52,6 @@ class TransactionValidatorBatchFailedTest {
         listAppender = new ListAppender<>();
         listAppender.start();
         testLogger.addAppender(listAppender);
-    }
-
-    @AfterEach
-    void tearDown() {
-        JdbcTestUtils.deleteFromTables(jdbcTemplate, "transaction");
     }
 
     @After
